6f2aedeca34ab0590c04d98645a4eb56dc20e3e0
Chenxi6132 <chenxix2@asu.edu>
2023-05-12 10:45:46 -0700

    Initial Commit

---
+++ catfoodCal/catfoodCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  catfoodCalApp.swift
+//  catfoodCal
+//
+//  Created by Chenxi Xu on 5/12/23.
+//
+
+import SwiftUI
+
+@main
+struct catfoodCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
