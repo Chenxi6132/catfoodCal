06a1d5e29b7c55f751c3ca6f6027c8b230474c8f
Chenxi6132 <chenxix2@asu.edu>
2023-05-12 11:11:54 -0700

    change text file

--- catfoodCal/ContentView.swift
+++ catfoodCal/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         
-        Text(/*@START_MENU_TOKEN@*/"Placeholder"/*@END_MENU_TOKEN@*/)
+        Text("Hello")
             .padding(.all)
             .background(Color.blue
                 .blur(radius: /*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/))
